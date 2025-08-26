89efd2e414c37aeb76b05531f0dfdd0c5245a268
bfmhno3 <858446559@qq.com>
2025-08-26 22:40:32 +0800

    feat: 实现静态顺序表的头插和尾插

---
+++ inc/static_sequential_list.hpp
@@ -0,0 +1,32 @@
+#ifndef STATIC_SEQUENTIAL_LIST_HPP__
+#define STATIC_SEQUENTIAL_LIST_HPP__
+
+namespace bfh {
+constexpr int CAPACITY = 1e3 + 10;
+
+class StaticSequentialList {
+ public:
+  StaticSequentialList() : size_(0) {};
+  ~StaticSequentialList() = default;
+  int capacity() { return CAPACITY; }
+  int size() { return size_; }
+  int get_data(int index) { return list_[index]; }
+  void push_back(int data) { list_[++size_] = data; }  // 第一个元素下标为 1
+  void push_front(int data) {
+    // 1. 先将现有的元素向后移动一位
+    for (int i = size_; i >= 1; i--) {
+      list_[i + 1] = list_[i];
+    }
+
+    // 2. 将数据放在表头
+    list_[1] = data;
+    size_++;
+  }
+
+ private:
+  int list_[CAPACITY];
+  int size_;
+};
+}  // namespace bfh
+
+#endif  // STATIC_SEQUENTIAL_LIST_HPP__
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "main.hpp"
+
+#include <iostream>
+
+#include "static_sequential_list.hpp"
+
+void print(bfh::StaticSequentialList& sqlist) {
+  int size = sqlist.size();
+  for (int i = 1; i <= size; i++) {
+    std::cout << sqlist.get_data(i) << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main(void) {
+  bfh::StaticSequentialList sqlist;
+  sqlist.push_back(2);
+  print(sqlist);
+  sqlist.push_back(5);
+  print(sqlist);
+  sqlist.push_back(1);
+  print(sqlist);
+  sqlist.push_back(3);
+  print(sqlist);
+  sqlist.push_front(10);
+  print(sqlist);
+  return 0;
+}
