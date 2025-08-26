8b6771d8925ddcb699cb041808eb08132d1a0fc7
bfmhno3 <858446559@qq.com>
2025-08-26 23:00:55 +0800

    feat: 添加对插入的参数安全检查

--- inc/static_sequential_list.hpp
+++ inc/static_sequential_list.hpp
@@ -14,6 +14,10 @@ class StaticSequentialList {
   void push_back(int data) { insert(data, size_ + 1); }  // 第一个元素下标为 1
   void push_front(int data) { insert(data, 1); }
   void insert(int data, int position) {
+    // 参数安全检查
+    if (position <= 0 || position > size_ + 1 || size_ >= CAPACITY - 1) {
+      return;
+    }
     // 将 position 及其后面的元素后移
     for (int i = size_; i >= position; i--) {
       list_[i + 1] = list_[i];
