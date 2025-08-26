0aab38acbfbda6a4b4e407bf3547d2992c74d16a
bfmhno3 <858446559@qq.com>
2025-08-26 22:52:13 +0800

    feat: 使用 insert 实现头插尾插，提高内聚

--- inc/static_sequential_list.hpp
+++ inc/static_sequential_list.hpp
@@ -6,32 +6,22 @@ constexpr int CAPACITY = 1e3 + 10;
 
 class StaticSequentialList {
  public:
-  StaticSequentialList() : size_(0) {};
+  StaticSequentialList() : list_{}, size_(0) {};
   ~StaticSequentialList() = default;
   int capacity() { return CAPACITY; }
   int size() { return size_; }
   int get_data(int index) { return list_[index]; }
-  void push_back(int data) { list_[++size_] = data; }  // 第一个元素下标为 1
-  void push_front(int data) {
-    // 1. 先将现有的元素向后移动一位
-    for (int i = size_; i >= 1; i--) {
-      list_[i + 1] = list_[i];
-    }
-
-    // 2. 将数据放在表头
-    list_[1] = data;
-    size_++;
-  }
+  void push_back(int data) { insert(data, size_ + 1); }  // 第一个元素下标为 1
+  void push_front(int data) { insert(data, 1); }
   void insert(int data, int position) {
     // 将 position 及其后面的元素后移
     for (int i = size_; i >= position; i--) {
-    
-    list_[i + 1] = list_[i];
+      list_[i + 1] = list_[i];
     }
 
     // 插入元素
     list_[position] = data;
-    
+
     size_++;
   }
 
